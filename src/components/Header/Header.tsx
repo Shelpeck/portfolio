d5e7e92b4589af1df1ef108dcc1bbeb57e9a1dbf
Yershat <yershhabdrashit@gmail.com>
2026-05-25 16:54:25 +0500

    navbar ui

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,18 +1,22 @@
 import EmailIcon from "@/assets/icons/EmailIcon"
 import GithubIcon from "@/assets/icons/GithubIcon"
+import LinkedinIcon from "@/assets/icons/LinkedinIcon"
+
+const list = "cursor-pointer"
 
 const Header = () => {
   return (
-    <nav className="flex">
+    <nav className="flex border-b-gray-400 border-2 ">
       <p className="text-5xl text-center">Portfolio</p>
-      <ul>
-        <li>About</li>
-        <li>Stack</li>
-        <li>Experience</li>
+      <ul className="flex flex-1 flex-row gap-4 items-center justify-end pr-10">
+        <li className={list}>About</li>
+        <li className={list}>Stack</li>
+        <li className={list}>Experience</li>
       </ul>
-      <ul>
-        <li><EmailIcon /></li>
+      <ul className="flex flex-row gap-1 items-center ">
         <li><GithubIcon /></li>
+        <li><LinkedinIcon /></li>
+        <li><EmailIcon /></li>
       </ul>
     </nav>
   )
