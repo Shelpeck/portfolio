fe0731576cd6d5d62c985e0f81fa0846be9a4672
Yershat <yershhabdrashit@gmail.com>
2026-05-25 17:39:31 +0500

    hero

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
+import Hero from "@/components/Hero/Hero";
+
 export default function Home() {
   return (
     <div className="flex flex-col flex-1 items-center justify-centerfont-sans">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 sm:items-start">
-
+      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 sm:items-start">
+        <Hero />
       </main>
     </div>
   );
