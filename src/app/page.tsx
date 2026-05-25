557167d59e390d275df52b18b7ce79303e16bedb
Yershat <yershhabdrashit@gmail.com>
2026-05-25 18:08:21 +0500

    techincal stack

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import Hero from "@/components/Hero/Hero";
+import TechnicalStack from "@/components/TechnicalStack/TechnicalStack";
 
 export default function Home() {
   return (
     <div className="flex flex-col flex-1 items-center justify-centerfont-sans">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 sm:items-start">
+      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 gap-20 sm:items-start">
         <Hero />
+        <TechnicalStack />
       </main>
     </div>
   );
